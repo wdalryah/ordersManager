7e7976c3b7745ecf243ade969cc4c82480bd3017
elmustafafriri <1234567Mm@>
2023-03-20 02:02:24 +0400

    order dto

--- src/main/java/com/demo/ordersManager/repositories/OrderRepositories.java
+++ src/main/java/com/demo/ordersManager/repositories/OrderRepositories.java
@@ -2,13 +2,15 @@ package com.demo.ordersManager.repositories;
 
 
 import com.demo.ordersManager.dto.Order;
+import com.demo.ordersManager.dto.OrderId;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @EnableScan
-public interface OrderRepositories extends  CrudRepository<Order, String> {
-    Optional<Order> findById(String id);
+public interface OrderRepositories extends  CrudRepository<Order, OrderId> {
+    List<Order> findById(String id);
 
 }
